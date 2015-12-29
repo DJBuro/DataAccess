e30b35261dc6345625532d09e979b45449d4de3f
Barry Jones <barry_jones@androtech.com>
2015-12-29 11:25:03 +0000

    Completed background image upload

---
+++ AndroAdminDataAccess/Domain/WebOrderingSetup/CustomThemeSettings.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AndroAdminDataAccess.Domain.WebOrderingSetup
+{
+    public class CustomThemeSettings
+    {
+        public void DefaultCustomThemeSettings()
+        {
+            DesktopBackgroundImagePath = string.Empty;
+            MobileBackgroundImagePath = string.Empty;
+        }
+        public string DesktopBackgroundImagePath { set; get; }
+        public string MobileBackgroundImagePath { set; get; }
+    }
+}
