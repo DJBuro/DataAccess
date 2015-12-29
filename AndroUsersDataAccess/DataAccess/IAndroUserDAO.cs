3ae61ae55e662ef83c1463a8580340b2d086b6d9
Barry Jones <barry_jones@androtech.com>
2015-12-29 08:02:37 +0000

    (TFS Integration from 'tfsvm (VC)' Id: 472;CreationDate: 2012-06-20 14:22:29 UTC;)

---
+++ AndroUsersDataAccess/DataAccess/IAndroUserDAO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AndroUsersDataAccess.Domain;
+
+namespace AndroUsersDataAccess.DataAccess
+{
+    public interface IAndroUserDAO
+    {
+        AndroUser GetByEmailAddress(string emailAddress);
+    }
+}
