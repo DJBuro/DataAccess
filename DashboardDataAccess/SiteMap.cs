5bca4380b6aa961d962c599f78bab2262efe5ffb
Barry Jones <barry_jones@androtech.com>
2015-12-29 07:53:29 +0000

    (TFS Integration from 'tfsvm (VC)' Id: 264;CreationDate: 2012-04-26 07:59:59 UTC;)

---
+++ DashboardDataAccess/SiteMap.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using FluentNHibernate.Mapping;
+
+namespace DashboardDataAccess
+{
+    public class SiteMap : ClassMap<Site>
+    {
+        public SiteMap()
+        {
+            Id(x => x.Id);
+            Map(x => x.SiteId);
+            Map(x => x.Name);
+            Map(x => x.HeadOfficeID);
+            Map(x => x.IPAddress);
+            Map(x => x.SiteTypeId);
+            Map(x => x.Enabled);
+            Map(x => x.SiteKey);
+            Map(x => x.RegionId);
+            Map(x => x.LastUpdated);
+            Map(x => x.Column_1);
+            Map(x => x.Column_2);
+            Map(x => x.Column_3);
+            Map(x => x.Column_4);
+            Map(x => x.Column_5);
+            Map(x => x.Column_6);
+            Map(x => x.Column_7);
+            Map(x => x.Column_8);
+            Map(x => x.Column_9);
+            Map(x => x.Column_10);
+            Map(x => x.Column_11);
+            Map(x => x.Column_12);
+            Map(x => x.Column_13);
+            Map(x => x.Column_14);
+            Map(x => x.Column_15);
+            Map(x => x.Column_16);
+            Map(x => x.Column_17);
+            Map(x => x.Column_18);
+            Map(x => x.Column_19);
+            Map(x => x.Column_20);
+            Map(x => x.Comp);
+            Map(x => x.Column_21);
+        }
+    }
+}
