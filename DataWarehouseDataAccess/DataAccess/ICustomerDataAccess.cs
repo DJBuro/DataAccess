52aa831f7f454d931917753edcf8e1b8dfb71a89
Barry Jones <barry_jones@androtech.com>
2015-12-29 10:33:04 +0000

    Update customer accounts

--- DataWarehouseDataAccess/DataAccess/ICustomerDataAccess.cs
+++ DataWarehouseDataAccess/DataAccess/ICustomerDataAccess.cs
@@ -12,6 +12,6 @@ namespace DataWarehouseDataAccess.DataAccess
         string GetByUsernamePassword(string username, string password, int applicationId, out DataWarehouseDataAccess.Domain.Customer customer);
         string Exists(string username, int applicationId, out bool exists);
         string AddCustomer(string username, string password, int applicationId, DataWarehouseDataAccess.Domain.Customer customer);
-        string UpdateCustomer(string username, string password, int applicationId, DataWarehouseDataAccess.Domain.Customer customer);
+        string UpdateCustomer(string username, string password, string newPassword, int applicationId, DataWarehouseDataAccess.Domain.Customer customer);
     }
 }
