b10026efa44097bf82ee7684545ce5747469d5d4
Barry Jones <barry_jones@androtech.com>
2015-12-29 09:31:55 +0000

    convert database to dev-sql-01

---
+++ AndroCloudDataAccess/Domain/HubItem.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AndroCloudDataAccess.Domain
+{
+    public class HubItem
+    {
+        /// <summary>
+        /// Gets or sets the id.
+        /// </summary>
+        /// <value>The id.</value>
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name.
+        /// </summary>
+        /// <value>The name.</value>
+        public string Name { get; set; }
+
+
+        /// <summary>
+        /// Gets or sets the address.
+        /// </summary>
+        /// <value>The address.</value>
+        public string Address { get; set; }
+
+        /// <summary>
+        /// Gets or sets the active flag.
+        /// </summary>
+        /// <value>The active.</value>
+        public bool Active { get; set; }
+
+    }
+}
