3e7bf0cdfd22ba41944dd41cadd01a47f4f3161f
Barry Jones <barry_jones@androtech.com>
2015-12-29 08:17:39 +0000

    (TFS Integration from 'tfsvm (VC)' Id: 741;CreationDate: 2012-09-17 23:58:26 UTC;)

--- AndroCloudDataAccess/DataAccess/IOpeningHoursDataAccess.cs
+++ AndroCloudDataAccess/DataAccess/IOpeningHoursDataAccess.cs
@@ -9,6 +9,7 @@ namespace AndroCloudDataAccess.DataAccess
     public interface IOpeningHoursDataAccess
     {
         string DeleteByIdMyAndromedaUserId(Guid openingHoursId, string myAndromedaUserId);
+        string DeleteBySiteIdDayMyAndromedaUserId(string externalSiteId, string day, string myAndromedaUserId);
         string AddByMyAndromedaUserId(TimeSpanBlock timeSpanBlock, string externalSiteId, string myAndromedaUserId);
     }
 }
