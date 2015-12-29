73d4b55f4c15a988ea6576f3c0a5eb08609c1d70
Barry Jones <barry_jones@androtech.com>
2015-12-29 11:12:08 +0000

    Sankeerthana - 01- 4598 - List, Create and Edit screens for Andro web ordering management

---
+++ AndroAdminDataAccess/Domain/AndroWebOrderingWebsite.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace AndroAdminDataAccess.Domain
+{
+    public class AndroWebOrderingSubscriptionType
+    {
+        public int Id { set; get; }
+        public string Subscription { set; get; }
+        public string Description { set; get; }
+        public int DisplayOrder { set; get; }
+    }
+     
+    public class AndroWebOrderingWebsite
+    {
+        public int Id { set; get; }
+        public string Name { set; get; }
+        public int? ChainId { set; get; }
+        public bool Enabled { set; get; }
+        public string Status { set; get; }
+        public string DisabledReason { set; get; }
+        public int SubscriptionTypeId { set; get; }
+        public string SubscriptionName { set; get; }
+        public string URL { set; get; }
+        public int ACSApplicationId { set; get; }
+        public int DataVersion { set; get; }
+        public ACSApplication ACSApplication { set; get; }
+        public IList<int> MappedSiteIds { set; get; }
+        public IList<Store> AllStores { set; get; }
+        public IList<Chain> Chains { set; get; }
+        public string StoresCount { set; get; }
+        public IList<AndroWebOrderingSubscriptionType> SubscriptionsList { set; get; }
+        public string UpdatedMappedStoreIds { set; get; }
+    }
+
+
+}
