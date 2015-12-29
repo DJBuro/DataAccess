a95facb088091e9d1c77b32a30c14abcd29447b5
Barry Jones <barry_jones@androtech.com>
2015-12-29 11:06:59 +0000

    Sankeerthana - 4663 - 01 - Data sync for deliveryzones

---
+++ AndroAdminDataAccess/EntityFramework/DataAccess/DeliveryAreaDataService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Data.Entity;
+
+
+namespace AndroAdminDataAccess.EntityFramework.DataAccess
+{
+    public class DeliveryAreaDataService
+    {
+        public IEnumerable<DeliveryArea> List(Expression<Func<DeliveryArea, bool>> query)
+        {
+            var results = Enumerable.Empty<DeliveryArea>();
+
+            using (var dbContext = new EntityFramework.AndroAdminEntities())
+            {
+                var table = dbContext.DeliveryAreas.Include(e => e.Store).Where(query);
+                results = table.ToArray();
+            }
+
+            return results;
+        }
+    }
+}
