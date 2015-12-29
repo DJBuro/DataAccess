dc88e2233c08fa8c986bc7b84d178bc97c11fe7e
Barry Jones <barry_jones@androtech.com>
2015-12-29 07:53:33 +0000

    (TFS Integration from 'tfsvm (VC)' Id: 270;CreationDate: 2012-04-26 15:11:18 UTC;)

---
+++ DashboardDataAccess/Log.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DashboardDataAccess
+{
+    public class Log
+    {
+        public virtual int Id { get; set; }
+        public virtual string StoreId {get; set;}
+        public virtual string Severity {get; set;}
+        public virtual string Message {get; set;}
+        public virtual string Method{get; set;}
+        public virtual string Source{get; set;}
+        public virtual DateTime Created { get; set; }
+
+        public static void Create(Log log)
+        {
+            tbl_Log.Create(log);
+        }
+    }
+}
