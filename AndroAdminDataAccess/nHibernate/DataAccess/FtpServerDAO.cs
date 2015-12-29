56495eba61d7e43eb26579b6129a87cb0c09eb2a
Barry Jones <barry_jones@androtech.com>
2015-12-29 07:54:32 +0000

    (TFS Integration from 'tfsvm (VC)' Id: 279;CreationDate: 2012-05-03 09:15:29 UTC;)

---
+++ AndroAdminDataAccess/nHibernate/DataAccess/FtpServerDAO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AndroAdminDataAccess.Domain;
+using AndroAdminDataAccess.DataAccess;
+using AndroAdminDataAccess.nHibernate.Mappings;
+
+namespace AndroAdminDataAccess.nHibernate.DataAccess
+{
+    public class FtpServerDAO : IFtpServerDAO
+    {
+    }
+}
