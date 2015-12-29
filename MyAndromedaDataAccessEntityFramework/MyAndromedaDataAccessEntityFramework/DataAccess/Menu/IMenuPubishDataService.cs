f77bc97b4512b7d98df8ce0ce0c1ee1bfb607eea
Barry Jones <barry_jones@androtech.com>
2015-12-29 11:17:38 +0000

    fixed deadlock;

--- MyAndromedaDataAccessEntityFramework/MyAndromedaDataAccessEntityFramework/DataAccess/Menu/IMenuPubishDataService.cs
+++ MyAndromedaDataAccessEntityFramework/MyAndromedaDataAccessEntityFramework/DataAccess/Menu/IMenuPubishDataService.cs
@@ -16,8 +16,22 @@ namespace MyAndromedaDataAccessEntityFramework.DataAccess.Menu
         /// <param name="status">The status.</param>
         void SetAcsUploadMenuDataTaskStatus(SiteMenu menu, TaskStatus status, DateTime? date = null);
 
+        /// <summary>
+        /// Gets the publish tasks.
+        /// </summary>
+        /// <param name="time">The time.</param>
+        /// <returns></returns>
         IEnumerable<SiteMenu> GetPublishTasks(DateTime time);
 
+        /// <summary>
+        /// Adds the history log.
+        /// </summary>
+        /// <param name="sitemenu">The sitemenu.</param>
+        /// <param name="userName">Name of the user.</param>
+        /// <param name="publishAll">The publish all.</param>
+        /// <param name="publishMenu">The publish menu.</param>
+        /// <param name="publishThumbnails">The publish thumbnails.</param>
+        /// <param name="publishOnUtc">The publish on UTC.</param>
         void AddHistoryLog(SiteMenu sitemenu, string userName, bool publishAll, bool publishMenu, bool publishThumbnails, DateTime? publishOnUtc);
     }
 
