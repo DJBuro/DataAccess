29e3843228aaca0b988d4b49548ef612e2c966b5
Barry Jones <barry_jones@androtech.com>
2015-12-29 10:56:58 +0000

    Voucher code validation rules

--- DataWarehouseDataAccess/DataAccess/IVoucherDataAccess.cs
+++ DataWarehouseDataAccess/DataAccess/IVoucherDataAccess.cs
@@ -11,5 +11,6 @@ namespace DataWarehouseDataAccess.DataAccess
         string ConnectionStringOverride { get; set; }
         string GetVouchersByAndromedaSiteId(int andromedaSiteId, out List<DataWarehouseDataAccess.Domain.VoucherCode> voucherList);
         string GetSingleVoucherByAndromedaSiteId(int andromedaSiteId, string voucherCode, out DataWarehouseDataAccess.Domain.VoucherCode voucherCodeObj);
+        string GetUsedVoucherCount(string voucherId, string customerId, out int usedVoucherCount);
     }
 }
