d3b1a68cbed1e0f91939dcb1870d46f170065c8b
Barry Jones <barry_jones@androtech.com>
2016-01-01 15:40:15 +0000

    (TFS Integration from 'tfsvm (VC)' Id: 503;CreationDate: 2012-07-04 16:23:44 UTC;)

--- AndroCloudDataAccess/Domain/Site.cs
+++ AndroCloudDataAccess/Domain/Site.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Xml.Serialization;
 
 namespace AndroCloudDataAccess.Domain
 {
     public class Site
     {
+        [XmlElement("Guid")]
         public Guid SiteGuid { get; set; }
         public string Name { get; set; }
         public int MenuVersion { get; set; }
