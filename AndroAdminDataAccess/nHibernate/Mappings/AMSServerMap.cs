6f90b1f3a5d9a94a86cfe6687672efa005dd9253
Barry Jones <barry_jones@androtech.com>
2016-01-01 15:36:52 +0000

    (TFS Integration from 'tfsvm (VC)' Id: 276;CreationDate: 2012-05-03 08:33:41 UTC;)

---
+++ AndroAdminDataAccess/nHibernate/Mappings/AMSServerMap.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FluentNHibernate.Mapping;
+using NHibernate;
+using DashboardDataAccess.Domain;
+
+namespace DashboardDataAccess.nHibernate.Mappings
+{
+    public class AMSServerMap : ClassMap<AMSServer>
+    {
+        public AMSServerMap()
+        {
+            Table("tbl_Log");
+            Id(x => x.Id);
+            Map(x => x.Name);
+            Map(x => x.Description);
+        }
+    }
+}
