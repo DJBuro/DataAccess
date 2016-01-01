9ce82fe45a3d0f678d0bd4923d7fdc0120865657
Barry Jones <barry_jones@androtech.com>
2016-01-01 16:51:00 +0000

    myAndromeda DataAccess changes

---
+++ MyAndromedaDataAccessEntityFramework/MyAndromedaDataAccessEntityFramework/DataAccess/CustomerDataAccess.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MyAndromedaDataAccess.DataAccess;
+
+namespace MyAndromedaDataAccessEntityFramework.DataAccess
+{
+    
+    public class CustomerDataAccess : ICustomerDataAccess 
+    {
+    }
+}
---
+++ MyAndromedaDataAccessEntityFramework/MyAndromedaDataAccessEntityFramework/DomainModelUpdateExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MyAndromedaDataAccessEntityFramework
+{
+    public static class DomainModelUpdateExtensions
+    {
+        public static MyAndromedaDataAccess.Domain.Marketing.EmailCampaign ToDomainModel(this Model.EmailCampaign entity)
+        {
+            var model = new MyAndromedaDataAccess.Domain.Marketing.EmailCampaign()
+            {
+                Id = entity.Id,
+                Title = entity.Title,
+                EmailTemplate = entity.EmailTemplate,
+                Created = entity.Created,
+                Modified = entity.Modified
+            };
+
+            return model;
+        }
+
+        public static void Update(this Model.EmailCampaign entity, MyAndromedaDataAccess.Domain.Marketing.EmailCampaign domainModel)
+        {
+            entity.Modified = DateTime.Now;
+            entity.Title = domainModel.Title;
+            entity.EmailTemplate = domainModel.EmailTemplate;
+        }
+    }
+}
