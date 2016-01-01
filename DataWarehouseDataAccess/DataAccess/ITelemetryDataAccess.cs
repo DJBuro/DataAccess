d182df09a5e0a96cfecc2c3d52df469b48a90757
Barry Jones <barry_jones@androtech.com>
2016-01-01 23:10:04 +0000

    Custom logging

---
+++ DataWarehouseDataAccess/DataAccess/ITelemetryDataAccess.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DataWarehouseDataAccess.Domain;
+
+namespace DataWarehouseDataAccess.DataAccess
+{
+    public interface ITelemetryDataAccess
+    {
+        string ConnectionStringOverride { get; set; }
+
+        string AddTelemetrySession
+        (
+            int applicationId,
+            DataWarehouseDataAccess.Domain.TelemetrySession telemetrySession,
+            string externalSiteId
+        );
+
+        string AddTelemetry
+        (
+            int applicationId,
+            DataWarehouseDataAccess.Domain.Telemetry telemetry
+        );
+    }
+}
