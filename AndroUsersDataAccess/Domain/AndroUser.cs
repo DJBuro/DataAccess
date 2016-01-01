6f880edbbb29758c118b7a4657cdb80f319dd6c0
Barry Jones <barry_jones@androtech.com>
2016-01-01 15:39:27 +0000

    (TFS Integration from 'tfsvm (VC)' Id: 472;CreationDate: 2012-06-20 14:22:29 UTC;)

---
+++ AndroUsersDataAccess/Domain/AndroUser.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+
+namespace AndroUsersDataAccess.Domain
+{
+    public class AndroUser
+    {
+        /// <summary>
+        /// Id
+        /// </summary>
+        public virtual int Id { get; set; }
+
+        /// <summary>
+        /// First name
+        /// </summary>
+        [Required(ErrorMessage = "Please enter a name")]
+        [StringLength(255, ErrorMessage = "Max 255 characters")]
+        public virtual string FirstName { get; set; }
+
+        /// <summary>
+        /// Surname
+        /// </summary>
+        [Required(ErrorMessage = "Please enter a name")]
+        [StringLength(255, ErrorMessage = "Max 255 characters")]
+        public virtual string SurName { get; set; }
+
+        /// <summary>
+        /// Password
+        /// </summary>
+        [Required(ErrorMessage = "Please enter a name")]
+        [StringLength(255, ErrorMessage = "Max 255 characters")]
+        public virtual string Password { get; set; }
+
+        /// <summary>
+        /// Email address
+        /// </summary>
+        [Required(ErrorMessage = "Please enter a name")]
+        [StringLength(255, ErrorMessage = "Max 255 characters")]
+        public virtual string EmailAddress { get; set; }
+
+        /// <summary>
+        /// Active
+        /// </summary>
+        public virtual bool Active { get; set; }
+
+        /// <summary>
+        /// Created
+        /// </summary>
+        [Required(ErrorMessage = "Please enter a name")]
+        [StringLength(255, ErrorMessage = "Max 255 characters")]
+        public virtual DateTime Created { get; set; }
+    }
+}
---
+++ AndroUsersDataAccess/nHibernate/Mappings/AndroUserMap.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FluentNHibernate.Mapping;
+using NHibernate;
+using AndroUsersDataAccess.Domain;
+
+namespace AndroUsersDataAccess.nHibernate.Mappings
+{
+    public class AndroUserMap : ClassMap<AndroUser>
+    {
+        public AndroUserMap()
+        {
+            Table("tbl_AndroUser");
+            Id(x => x.Id);
+            Map(x => x.FirstName);
+            Map(x => x.SurName);
+            Map(x => x.Password);
+            Map(x => x.EmailAddress);
+            Map(x => x.Active);
+            Map(x => x.Created);
+
+        }
+    }
+}
