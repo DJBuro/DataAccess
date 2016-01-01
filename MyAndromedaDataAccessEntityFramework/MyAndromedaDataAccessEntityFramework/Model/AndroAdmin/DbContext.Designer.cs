9f196b7713320a8b0aa76f47d068bb885b22352a
Barry Jones <barry_jones@androtech.com>
2016-01-01 20:27:52 +0000

    localization - store changes.

--- MyAndromedaDataAccessEntityFramework/MyAndromedaDataAccessEntityFramework/Model/AndroAdmin/DbContext.Designer.cs
+++ MyAndromedaDataAccessEntityFramework/MyAndromedaDataAccessEntityFramework/Model/AndroAdmin/DbContext.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\TFS-workspace\Rameses Web Components\DataAccess\MyAndromedaDataAccessEntityFramework\MyAndromedaDataAccessEntityFramework\Model\AndroAdmin\DbContext.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Andromeda Development Source\Rameses Web Components\DataAccess\MyAndromedaDataAccessEntityFramework\MyAndromedaDataAccessEntityFramework\Model\AndroAdmin\DbContext.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
