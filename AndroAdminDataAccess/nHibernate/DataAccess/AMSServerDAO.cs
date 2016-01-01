51aeb079451ab88a77bbd7fea72b607b7772b0be
Barry Jones <barry_jones@androtech.com>
2016-01-01 15:37:01 +0000

    (TFS Integration from 'tfsvm (VC)' Id: 279;CreationDate: 2012-05-03 09:15:29 UTC;)

---
+++ AndroAdminDataAccess/nHibernate/DataAccess/AMSServerDAO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AndroAdminDataAccess.Domain;
+using AndroAdminDataAccess.DataAccess;
+using AndroAdminDataAccess.nHibernate.Mappings;
+
+namespace AndroAdminDataAccess.nHibernate.DataAccess
+{
+    public class AMSServerDAO : IAMSServerDAO
+    {
+    }
+}
---
+++ AndroAdminDataAccess/nHibernate/DataAccess/DataAccess.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AndroAdminDataAccess.Domain;
+using AndroAdminDataAccess.DataAccess;
+using AndroAdminDataAccess.nHibernate;
+
+namespace AndroAdminDataAccess.nHibernate.DataAccess
+{
+    public class DataAccess : IDataAccess
+    {
+        //public static void UpdateDashboardData(Site site, HistoricalData historicalData, bool isHistoricalDataNew)
+        //{
+        //    nHibernateDataAccess.UpdateDashboardData(site, historicalData, isHistoricalDataNew);
+        //}
+    }
+}
