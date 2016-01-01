966a978b33d8e0e9f3aef1046c16241d13dc61e0
Barry Jones <barry_jones@androtech.com>
2016-01-01 21:12:36 +0000

    accidently rebuilt all t4 files ...
    and included are postcode radius search changes

--- MyAndromedaDataAccessEntityFramework/MyAndromedaDataAccessEntityFramework/Model/AndroAdmin/DbContext.Context.cs
+++ MyAndromedaDataAccessEntityFramework/MyAndromedaDataAccessEntityFramework/Model/AndroAdmin/DbContext.Context.cs
@@ -62,7 +62,8 @@ namespace MyAndromedaDataAccessEntityFramework.Model.AndroAdmin
         public DbSet<AndroWebOrderingSubscriptionType> AndroWebOrderingSubscriptionTypes { get; set; }
         public DbSet<AndroWebOrderingWebsite> AndroWebOrderingWebsites { get; set; }
         public DbSet<AndroWebOrderingTheme> AndroWebOrderingThemes { get; set; }
-
+        public DbSet<Site_AMS_upload> Site_AMS_upload { get; set; }
+        public DbSet<sysdiagram> sysdiagrams { get; set; }
         public DbSet<DeliveryZoneName> DeliveryZoneNames { get; set; }
         public DbSet<PostCodeSector> PostCodeSectors { get; set; }
     }
