91c0d27d352e33d810c0c553413c0bb389f83491
Barry Jones <barry_jones@androtech.com>
2016-01-02 01:31:51 +0000

    datawarehouse changes ef6

--- DataWarehouseDataAccessEntityFramework/DataAccess/VoucherDataAccess.cs
+++ DataWarehouseDataAccessEntityFramework/DataAccess/VoucherDataAccess.cs
@@ -5,7 +5,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
-using System.Data.Objects;
 
 namespace DataWarehouseDataAccessEntityFramework.DataAccess
 {
